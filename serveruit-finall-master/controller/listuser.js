19c413f88905b7aeab6bf9f13fdb9fee4032fe06
kidzer0 <dinhtrunghau.kid@gmail.com>
2020-05-30 17:00:48 +0700

    list user

--- serveruit-finall-master/controller/listuser.js
+++ serveruit-finall-master/controller/listuser.js
@@ -1,10 +1,11 @@
 var User = require('../model/User');
 
-exports.getDataGC= async (req, res, next) => {
+exports.listuser= async (req, res, next) => {
+    console.log("aaaa")
     User.find().sort({_id: -1 })
     .exec(function (err, data) {
         if (err) return handleError(err);
         res.json(data);
-        // console.log(data);
+        console.log(data);
     })
 }
