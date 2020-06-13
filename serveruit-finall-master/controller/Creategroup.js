935ecb6340887889283bf080e4992f521399fcf8
hieunt <tronghieu8426@gmail.com>
2020-06-14 04:29:16 +0700

    fix ducicate create group

--- serveruit-finall-master/controller/Creategroup.js
+++ serveruit-finall-master/controller/Creategroup.js
@@ -7,11 +7,13 @@ exports.CreategroupChat = async (req, res, next) => {
     listMssv: data.listMssv,
   })
   // res.json(newGroup);
+  console.log("Create group", newGroup.groupName)
   Group.find({ groupName: data.groupName })
     .exec(function (err, data) {
+      console.log("data", data);
       if (err) return handleError(err);
-      if (!data.length) {
-        console.log("Create group", newGroup.groupName)
+      if (data) {
+        console.log("Crp", newGroup.groupName)
         newGroup.save(function (err) {
           if (err) throw err;
         })
